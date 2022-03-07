19a083f9a972270b2a008cd5922e7763dd1c5c04
Ajay Kumar Kasam <kasamajay at gmail dot com>
2022-03-07 06:50:11 +0000

    empty

--- vars/fullPipeline.groovy
+++ vars/fullPipeline.groovy
@@ -7,8 +7,20 @@ def call() {
             stage('test') {
                 steps {
                     echo 'Hello'
+                    script {
+                        test1()
+                    }
                 }
             }
         }
     }
+}
+
+def test1() {
+    echo 'test1'
+    test2()
+}
+
+def test2() {
+    echo 'test2'
 }
