e3063b89c18512a4060f783314609583c08a4783
Ajay Kumar Kasam <kasamajay at gmail dot com>
2022-03-07 05:48:39 +0000

    empty

--- vars/fullPipeline.groovy
+++ vars/fullPipeline.groovy
@@ -1,12 +1,3 @@
 def call() {
-    pipeline {
-        agent { label any }
-        stages {
-            stage('checkout') {
-                steps {
-                    sh "echo 'Hello'"
-                }
-            }
-        }
-    }
+    sh "echo 'Hello'"
 }
