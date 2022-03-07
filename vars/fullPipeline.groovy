a96661f671d8142565dd706e395305686c3630c3
Ajay Kumar Kasam <kasamajay at gmail dot com>
2022-03-07 06:37:45 +0000

    empty

--- vars/fullPipeline.groovy
+++ vars/fullPipeline.groovy
@@ -1,3 +1,14 @@
-def call(Map config = [:]) {
+def call(Map config = [:], Closure body = {}) {
+    tmpCfg = createConfiguration(body)
     
+    pipeline {
+        agent any
+        stages {
+            stage {
+                steps {
+                    echo 'Hello'
+                }
+            }
+        }
+    }
 }
